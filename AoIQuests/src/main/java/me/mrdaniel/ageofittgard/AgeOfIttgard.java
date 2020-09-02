aca4d3fef388925e295cb0d4f861f12d3280046f
Daniel <danielpeters9797@gmail.com>
2020-09-02 17:06:13 +0200

    Named project

--- AoIQuests/src/main/java/me/mrdaniel/ageofittgard/AgeOfIttgard.java
+++ AoIQuests/src/main/java/me/mrdaniel/ageofittgard/AgeOfIttgard.java
@@ -71,8 +71,8 @@ import java.nio.file.Path;
         dependencies = { @Dependency(id = "npcs", version = "[4,)") })
 public class AgeOfIttgard {
 
-    public static final String MOD_ID = "ageofittgard";
-    public static final String NAME = "AgeOfIttgard";
+    public static final String MOD_ID = "aoi-quests";
+    public static final String NAME = "AgeOfIttgard Quests";
     public static final String VERSION = "1.0.0";
     public static final String DESCRIPTION = "The Age Of Ittgard quest plugin.";
 
